3100e0ba85e522aa5de40787136c472088f911c1
Freddiefady <dodofady49@gmail.com>
2025-07-27 01:54:49 +0300

    add migration files and scheme

---
+++ database/migrations/2025_07_26_145623_create_group_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('role', 25);
+            $table->string('status', 25); // e.g., 'approved', 'pending', 'banned'
+            $table->string('token', 1024)->nullable();
+            $table->string('token_expire_date', 1024)->nullable();
+            $table->string('token_used', 1024)->nullable();
+            $table->foreignId('group_id')->constrained('groups');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_users');
+    }
+};
