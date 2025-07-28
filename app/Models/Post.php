ef48eca0aacca69d534dd6f9f1f0b3e5a574aecc
Freddiefady <dodofady49@gmail.com>
2025-07-28 23:48:04 +0300

    Implement slug generation for User model and update Post model relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'body',
+        'image',
+        'user_id',
+        'group_id',
+        'deleted_by',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function deletedBy()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
 }
