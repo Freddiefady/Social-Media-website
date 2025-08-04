cdc6cd109a7229010009f46fb599817016c0cb20
Freddiefady <dodofady49@gmail.com>
2025-08-04 23:50:34 +0300

    feat: create files profile and bind uri of username

---
+++ resources/js/Pages/Profile/View.vue
@@ -0,0 +1,11 @@
+<script>
+import { usePage } from '@inertiajs/vue3';
+
+const user = usePage.props.auth.user;
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style lang="scss" scoped></style>
