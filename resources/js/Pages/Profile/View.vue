c696865b74b8cc86d32374ab818864426fa865ce
Freddiefady <dodofady49@gmail.com>
2025-08-06 17:55:37 +0300

    style: format and clean up code in Profile View component

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -1,11 +1,11 @@
 <script setup>
-import {TabGroup, TabList, Tab, TabPanels, TabPanel} from '@headlessui/vue'
-import {usePage} from '@inertiajs/vue3';
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+import { usePage } from '@inertiajs/vue3';
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import TabItem from "@/Pages/Profile/Partials/TabItem.vue";
 import Edit from "@/Pages/Profile/Edit.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import {computed} from "vue";
+import { computed } from "vue";
 
 const authUser = usePage().props.auth.user;
 const isMyProfile = computed(() => authUser && authUser.id === props.user.id)
@@ -28,27 +28,39 @@ const props = defineProps({
         <div class="w-[768px] mx-auto h-full overflow-auto">
             <div class="relative bg-white">
                 <img src="https://www.prodraw.net/fb_cover/images/fb_cover_65.jpg"
-                     class="w-full h-[200px] object-cover"/>
-               <div class="flex">
-                   <img src="https://konstantoulakis-chroma.gr/wp-content/uploads/2020/12/user3.jpg"
+                    class="w-full h-[200px] object-cover" />
+                <button class="absolute top-2 right-2 bg-gray-500 hover:bg-gray-100 text-gray-800 text-xs py-1 px-2 flex items-center opacity-0 group-hover:opacity-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-3 h-3 mr-2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+                    </svg>
+                    Upload Cover Image
+                    <input type="file" class="absolute top-0 left-0 right-0 bottom-0 opacity-0 cursor-pointer" />
+                </button>
+                <div class="flex">
+                    <img src="https://konstantoulakis-chroma.gr/wp-content/uploads/2020/12/user3.jpg"
                         class="ml-[128px] h-[128px] left-[48px] -mt-[64px]" />
-                   <div class="flex justify-between items-center flex-1 p-4">
-                       <h2 class="text-bold text-lg" v-if="authUser">{{ authUser.name }}</h2>
-                       <PrimaryButton v-if="isMyProfile">
-                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-2">
-                               <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-                           </svg>
-
-                           Edit Profile
-                       </PrimaryButton>
-                   </div>
-               </div>
+                    <div class="flex justify-between items-center flex-1 p-4">
+                        <h2 class="text-bold text-lg" v-if="authUser">{{ authUser.name }}</h2>
+                        <PrimaryButton v-if="isMyProfile">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="w-4 h-4 mr-2">
+                                <path
+                                    d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                            </svg>
+                            Edit Profile
+                        </PrimaryButton>
+                    </div>
+                </div>
             </div>
             <div class="border-t">
                 <TabGroup>
                     <TabList class="pl-[200px] flex bg-white">
                         <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                           <TabItem text="About" :selected="selected" />
+                            <TabItem text="About" :selected="selected" />
                         </Tab>
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Posts" :selected="selected" />
@@ -66,19 +78,19 @@ const props = defineProps({
 
                     <TabPanels class="mt-2">
                         <TabPanel v-if="isMyProfile">
-                           <Edit :must-verify-email="must-verify-email" :status="status" />
+                            <Edit :must-verify-email="must - verify - email" :status="status" />
                         </TabPanel>
                         <TabPanel class='bg-white p-3 shadow'>
                             Posts Content
                         </TabPanel>
                         <TabPanel class='bg-white p-3 shadow'>
-                           Followers Contnent
+                            Followers Contnent
                         </TabPanel>
                         <TabPanel class='bg-white p-3 shadow'>
-                           Followings Contnent
+                            Followings Contnent
                         </TabPanel>
                         <TabPanel class='bg-white p-3 shadow'>
-                           Photos
+                            Photos
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
