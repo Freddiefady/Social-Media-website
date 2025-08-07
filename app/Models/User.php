c815f0e3fd2f2bdd1fecda869b56dd3301bbc80e
Freddiefady <dodofady49@gmail.com>
2025-08-07 19:53:55 +0300

    feat: enable JSON resource wrapping and add cover_path and avatar_path to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'username',
         'email',
+        'cover_path',
+        'avatar_path',
         'password',
     ];
 
