81a2e2ffc198d8f1abc793726cb10f7a4319f483
Freddiefady <dodofady49@gmail.com>
2025-08-15 19:37:09 +0300

    feat: add size column to post attachments and update model fillable attributes

--- app/Models/PostAttachment.php
+++ app/Models/PostAttachment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostAttachment extends Model
 {
-    //
+    CONST UPDATED_AT = null;
+
+    protected $fillable = [
+        'post_id',
+        'created_by',
+        'name',
+        'path',
+        'mime',
+        'size',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
---
+++ database/migrations/2025_08_14_142754_add_size_column_to_post_attachments.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('post_attachments', function (Blueprint $table) {
+            $table->integer('size')->after('mime');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('post_attachments', function (Blueprint $table) {
+            $table->dropColumn('size');
+        });
+    }
+};
