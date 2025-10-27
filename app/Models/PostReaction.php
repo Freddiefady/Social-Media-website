5a5485235ac94259401ce15f610b22ccac45d72c
Freddiefady <dodofady49@gmail.com>
2025-10-28 00:22:53 +0300

    chore: refactor by PHPStan

--- app/Models/PostReaction.php
+++ app/Models/PostReaction.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property-read int $id
+ * @property-read int $user_id
+ * @property-read int $post_id
+ * @property-read string $type
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ * @property-read User $user
+ * @property-read Post $post
+ */
 final class PostReaction extends Model
 {
-    public const UPDATED_AT = null;
+    public const null UPDATED_AT = null;
 
     protected $fillable = [
         'user_id',
@@ -16,12 +28,18 @@ final class PostReaction extends Model
         'type',
     ];
 
-    public function user()
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function post()
+    /**
+     * @return BelongsTo<Post, $this>
+     */
+    public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
     }
