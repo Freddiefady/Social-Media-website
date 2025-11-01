25aa0255c8a416d459148cb74c0aecbb70176be0
Freddiefady <dodofady49@gmail.com>
2025-11-01 15:47:07 +0200

    fix: adjust placeholder in component & remove width full in butto.

--- resources/js/Components/app/IndigoButton.vue
+++ resources/js/Components/app/IndigoButton.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
     <button
         :type="type"
-        class="flex items-center justify-center w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        class="flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
         <slot></slot>
     </button>
