bfa28516dd2188d1d52edd38be146337dd434c1e
Freddiefady <dodofady49@gmail.com>
2025-11-05 00:52:20 +0200

    chore: correct relation in status and role.

---
+++ app/Notifications/GroupJoinRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class GroupJoinRequest extends Notification
+{
+    public function __construct(private $group)
+    {
+    }
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('User "'.$notifiable->name.'" requested to joined the group "'.$this->group->name.'"')
+            ->action('Approve Group Request', route('group.show', $this->group))
+            ->line('Thank you for using our application!');
+    }
+
+    public function toArray($notifiable): array
+    {
+        return [];
+    }
+}
