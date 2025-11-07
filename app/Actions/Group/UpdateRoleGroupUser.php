a41f438ad9a91ddd3b8484e0061b8b494131cd2f
Freddiefady <dodofady49@gmail.com>
2025-11-07 02:40:13 +0200

    feat: update specific role form request.

---
+++ app/Actions/Group/UpdateRoleGroupUser.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Group;
+
+use App\Models\GroupUser;
+
+final readonly class UpdateRoleGroupUser
+{
+    public function handle(GroupUser $groupUser, array $data): bool
+    {
+        return $groupUser->update(['role' => $data['role']]);
+    }
+}
