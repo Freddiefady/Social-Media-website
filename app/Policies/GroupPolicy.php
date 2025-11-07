0eba5de6be36fafcfd653f16ee793357d4b1f85a
Freddiefady <dodofady49@gmail.com>
2025-11-07 02:31:21 +0200

    chore: adjust permission for use group.

--- app/Policies/GroupPolicy.php
+++ app/Policies/GroupPolicy.php
@@ -4,14 +4,23 @@ namespace App\Policies;
 
 use App\Models\Group;
 use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Container\Attributes\CurrentUser;
 
 class GroupPolicy
 {
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Group $group): bool
-    {
-        return $group->users()->wherePivot('user_id', $user->id)->exists();
+    public function viewAny(
+        #[CurrentUser] User $user,
+        Group $group
+    ): Response {
+        return $group->adminUsers()
+            ->wherePivot('user_id', $user->id)
+            ->wherePivot('group_id', $group->id)
+            ->exists()
+            ? Response::allow()
+            : Response::deny('you don\'t have permission to perform this action', 403);
     }
 }
