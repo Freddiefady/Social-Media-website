98c3479b64fce3f83907f302d6ab47f0b3ab8aeb
Freddiefady <dodofady49@gmail.com>
2025-11-10 07:31:28 +0200

    chore: add notification to send to user is deleted.

---
+++ app/Actions/GroupUser/SendDeleteUser.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\GroupUser;
+
+use App\Models\GroupUser;
+use App\Notifications\DeletedUserFromGroup;
+
+final class SendDeleteUser
+{
+    public function handle(GroupUser $groupUser): void
+    {
+        $groupUser->creator->notify(new DeletedUserFromGroup($groupUser->group));
+    }
+}
---
+++ app/Notifications/DeletedUserFromGroup.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use App\Models\Group;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+final class DeletedUserFromGroup extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private readonly Group $group)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line("You have been removed from group {$this->group->name}.")
+            ->action('Open Group', route('group.show', $this->group))
+            ->line('Thank you for using our application!');
+    }
+}
