f5663575ace367e370074b3348e76f117aa43eee
Freddiefady <dodofady49@gmail.com>
2025-11-11 22:39:09 +0200

    feat: add send notification on Created Comment.

--- app/Actions/Comments/CreateComment.php
+++ app/Actions/Comments/CreateComment.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace App\Actions\Comments;
 
+use App\Actions\Notifications\SendCreateComment;
 use App\Models\Comment;
 use App\Models\Post;
 
 final readonly class CreateComment
 {
+    public function __construct(private SendCreateComment $sendCreateComment) {}
+
     public function handle(Post $post, string $content, ?int $parentId): Comment
     {
-        return $post->comments()->create([
+        $comment = $post->comments()->create([
             'user_id' => auth()->id(),
             'comment' => $content,
             'parent_id' => $parentId,
         ]);
+
+        $this->sendCreateComment->handle($comment);
+
+        return $comment;
     }
 }
---
+++ app/Notifications/CommentCreated.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+
+final class CommentCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private readonly Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $comment = Str::words($this->comment->comment, 5);
+
+        return (new MailMessage)
+            ->greeting('Hello My Friend!')
+            ->line('New comment was added on your post. Please see your comment below')
+            ->line("$comment")
+            ->action('View Post', url('/'))
+            ->line('Thank you for using our application!');
+    }
+}
