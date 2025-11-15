d42195b07fb5ea3c8531d364ba9e53237ac4dfed
Freddiefady <dodofady49@gmail.com>
2025-11-15 19:38:54 +0200

    fix: removed attributes used twice.

--- app/Actions/Group/CreateGroup.php
+++ app/Actions/Group/CreateGroup.php
@@ -13,10 +13,7 @@ final class CreateGroup
      */
     public function handle(array $data): Group
     {
-        return Group::query()->create([
-            'name' => $data['name'],
-            'about' => $data['about'],
-            'auto_approval' => $data['auto_approval'],
+        return Group::query()->create($data + [
             'user_id' => auth()->id(),
         ]);
     }
