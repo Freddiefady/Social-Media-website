6df3f5df6d8095ca200cb7b6f61d1ebec29cbb2c
Freddiefady <dodofady49@gmail.com>
2025-11-15 02:10:38 +0200

    chore(search): added if have not photo display message.

--- resources/js/Pages/Profile/TabPhoto.vue
+++ resources/js/Pages/Profile/TabPhoto.vue
@@ -19,26 +19,32 @@ function openPhoto(index) {
 </script>
 
 <template>
-<div class="grid gap-2 grid-cols-2 sm:grid-cols-3">
-    <template v-for="(attachment, ind) of photos">
-        <div @click="openPhoto(ind)" class="group bg-blue-100 aspect-square flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
+    <div class="grid gap-2 grid-cols-2 sm:grid-cols-3">
+        <template v-for="(attachment, ind) of photos">
+            <div @click="openPhoto(ind)"
+                 class="group bg-blue-100 aspect-square flex flex-col items-center justify-center text-gray-500 relative cursor-pointer">
 
-            <!-- Download -->
-            <a @click.stop :href="route('post.download', attachment)" class="z-20 w-8 h-8 flex flex-col items-center justify-center rounded bg-gray-700 hover:bg-gray-800 text-gray-100 absolute right-2 top-2 cursor-pointer group opacity-0 group-hover:opacity-100 transition-all">
-                <ArrowDownTrayIcon class="w-4 h-4" />
-            </a>
-            <!-- /Download -->
-            <img v-if="isImage(attachment)" :src="attachment.url" class="object-contain aspect-square" alt="" />
+                <!-- Download -->
+                <a @click.stop :href="route('post.download', attachment)"
+                   class="z-20 w-8 h-8 flex flex-col items-center justify-center rounded bg-gray-700 hover:bg-gray-800 text-gray-100 absolute right-2 top-2 cursor-pointer group opacity-0 group-hover:opacity-100 transition-all">
+                    <ArrowDownTrayIcon class="w-4 h-4"/>
+                </a>
+                <!-- /Download -->
+                <img v-if="isImage(attachment)" :src="attachment.url" class="object-contain aspect-square" alt=""/>
 
-            <div v-else class="flex items-center justify-center">
-                <PaperClipIcon class="w-4 h-4 mr-2" />
-                <small>{{ attachment.name }}</small>
+                <div v-else class="flex items-center justify-center">
+                    <PaperClipIcon class="w-4 h-4 mr-2"/>
+                    <small>{{ attachment.name }}</small>
+                </div>
             </div>
-        </div>
-    </template>
+        </template>
+    </div>
 
-    <AttachmentPreviewModal :attachments="photos || []" v-model:index="currentPhotoIndex" v-model="showModal" />
-</div>
+    <div v-if="!photos.length" class="text-center py-8 text-gray-500">
+        There is no Photos for this page.
+    </div>
+
+    <AttachmentPreviewModal :attachments="photos || []" v-model:index="currentPhotoIndex" v-model="showModal"/>
 </template>
 
 <style>
