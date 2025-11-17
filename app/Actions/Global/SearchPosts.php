3177c12906eac4aa9283eb4235b4fccf60f890da
Freddiefady <dodofady49@gmail.com>
2025-11-17 19:21:16 +0200

    fix(search): loaded reaction on post and reaction on comments.

--- app/Actions/Global/SearchPosts.php
+++ app/Actions/Global/SearchPosts.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace App\Actions\Global;
 
 use App\Models\Post;
+use App\Queries\PostRelatedReactionAndComments;
 use Illuminate\Pagination\LengthAwarePaginator;
 
-final class SearchPosts
+final readonly class SearchPosts
 {
+    public function __construct(private PostRelatedReactionAndComments $query) {}
+
     /**
      * @return LengthAwarePaginator<int, Post>
      */
     public function handle(string $keywords): LengthAwarePaginator
     {
-        return Post::query()
+        return $this->query->builder()
             ->whereLike('body', "%$keywords%")
-            ->latest()
             ->paginate(20);
     }
 }
