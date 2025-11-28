a366345eaa42075a96c6ae75786b7327e6db7c67
Freddiefady <dodofady49@gmail.com>
2025-11-29 01:31:28 +0200

    fix(eage-loading): missing eager user.

--- app/Actions/Group/FirstApprovedRequests.php
+++ app/Actions/Group/FirstApprovedRequests.php
@@ -24,6 +24,7 @@ final readonly class FirstApprovedRequests
     public function handle(Group $group, array $data): array
     {
         $query = GroupUser::query()
+            ->with('user:id,name')
             ->where('user_id', $data['user_id'])
             ->where('group_id', $group->id)
             ->where('status', GroupUserStatusEnum::PENDING->value)
